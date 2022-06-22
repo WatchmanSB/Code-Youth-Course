3ae9c859d1e8008a2d95dfd0e9dee0564e5fa648
WatchmanSB <69556611+WatchmanSB@users.noreply.github.com>
2022-06-22 09:04:29 -0400

    Update Carter_Assingment1.js

--- m1_fundamentals/JS/Carter_Assingment1.js
+++ m1_fundamentals/JS/Carter_Assingment1.js
@@ -1,3 +1,5 @@
+// Assignment 1
+
 let inputA= 5
 let inputB= 2
 
@@ -8,3 +10,14 @@ console.log("Is inputA greater than inputB?")
 if (inputA>inputB) {
     console.log ("InputA, is infact, greater than inputB")
 }
+
+
+// Assignment 2
+
+let studentmark= math.floor(math.random()*100)
+let passinggrade = 70
+
+
+
+
+// assignment 3
