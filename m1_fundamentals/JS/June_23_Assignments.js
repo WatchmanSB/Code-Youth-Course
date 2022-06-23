7fa43fe936645800fd87bea86fd8b8e9e17cb391
WatchmanSB <69556611+WatchmanSB@users.noreply.github.com>
2022-06-23 13:10:52 -0400

    updated

---
+++ m1_fundamentals/JS/June_23_Assignments.js
@@ -0,0 +1,79 @@
+
+// Count up to 10
+
+for (let i = 1; i <= 10; i++) {
+    console.log ("i:", i)
+}
+
+console.log ("break")
+
+
+
+
+// Count down to 10
+
+for (let a = 10; a >= 1; a--) {
+    console.log ("a:", a)
+}
+
+console.log ("break")
+
+
+
+
+// Count up by 2 from 0-10
+
+for (let b = 0; b <=10; b += 2) {
+    console.log ("b", b)
+}
+
+console.log ("break")
+
+
+
+
+
+// Count down by 2 from 10-0
+
+for (let c = 10; c >=0; c -= 2) {
+    console.log ("c", c)
+}
+
+console.log ("break")
+
+
+
+
+// Carter's Assignment
+
+
+for (let i = 0; i < 3 ; i ++) { switch(i) {
+    case 0: 
+        document.write ("<li><ul>1</ul><ul>34</ul><ul>230</ul></li>")
+    break; 
+
+    case 1:
+        document.write ("<li><ul>5</ul><ul>78</ul><ul>139</ul></li>") 
+    break;
+
+    case 2:
+        document.write ("<li><ul>8</ul><ul>15</ul><ul>570</ul></li>")
+    break;
+
+    case 3:
+        document.write ("<li><ul>2</ul><ul>65</ul><ul>310</ul></li>") 
+    break;
+
+    case 4:
+        document.write ("<li><ul>7</ul><ul>93</ul><ul>385</ul></li>")
+    break;
+
+    case 5:
+        document.write ("<li><ul>9</ul><ul>23</ul><ul>756</ul></li>")
+    break;
+}
+}
+
+
+
+
