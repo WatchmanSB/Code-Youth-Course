a0677559141339f057d9affbff1d4284feee46df
WatchmanSB <69556611+WatchmanSB@users.noreply.github.com>
2022-06-24 12:51:34 -0400

    June 24th work for the day

---
+++ m1_fundamentals/JS/June_24_js.js
@@ -0,0 +1,64 @@
+// examples 1: sum of natural numbers
+
+let sum = 0;
+
+for (let i = 1; i <= 15; i++) {
+    sum= sum + i;
+    console.log(`sum:${sum}, i: ${i}`)
+}
+console.log (`sum of 10 nautral numbers: ${sum}`)
+
+console.log ("break")
+
+
+// Assignment 1: sum factorial
+
+let fact = 1;
+for(let a = 6; a>=1; a--) {
+    fact = fact * a;
+    console.log ("fact:", fact, "a:", a);
+}
+
+console.log ("break")
+
+// Assignment 2: 
+
+let k = 0;
+while (k <= 10) {
+    console.log(k);
+    k++;
+}
+
+
+console.log ("break")
+
+
+
+// Assignment 3:
+let mult = 2
+for (let s = 1; s<=10; s++) {
+    console.log (mult*s)
+}
+
+
+//  Majid's Assignment
+
+let city = "Ottawa"
+for (let o = 1; o<=10; o++) {
+    console.log (city)
+}
+
+
+let dog = "Gina, Oscar, Bella";
+let d = 1
+while (d <= 5) {
+    console.log(dog);
+    d++;
+}
+
+
+
+let half = 0 
+for (let i = 2; i <=10; i++) {
+    console.log(i/2)
+}
