4362cdc7c07df3f92da61000b4900bfd9cc702a7
WatchmanSB <69556611+WatchmanSB@users.noreply.github.com>
2022-06-27 12:13:58 -0400

    June 27th work for the day

---
+++ m1_fundamentals/JS/June_27_assignments.js
@@ -0,0 +1,85 @@
+// Assignment 1
+
+console.log ("Assignment 1")
+
+
+
+function nam (first , last) {   
+    console.log ("First name:", first);
+    console.log ("Last name:", last);
+}
+
+let first = prompt("Enter first name")
+let last = prompt("Enter last name")
+
+nam (first,last);
+
+
+
+
+
+console.log ("break")
+
+
+
+
+
+
+function month (mnth , day) {
+    console.log ("Current month:", mnth)
+    console.log ("Current day:", day)
+}
+
+let mnth = prompt("Enter current month")
+let day = prompt("Enter current day")
+
+month (mnth,day)
+
+
+
+
+
+console.log ("break")
+
+
+
+
+
+function place (city, prov) {
+    console.log ("City:", city)
+    console.log ("Province:", prov)
+}
+
+let city= prompt("Enter your city")
+let prov= prompt("Enter your Province")
+
+place(city,prov)
+
+// Assignment 2
+
+
+console.log ("Assignment 2")
+
+function welcome (a, b) {
+    console.log (a,b, "I am Steven")
+}
+
+welcome ("Hello", "world")
+
+
+
+
+console.log ("Break")
+
+
+function divide (a) {
+    let b= a/2
+    console.log ("Your number divided by 2 is:" + b)
+}
+
+divide (10)
+
+
+
+
+
