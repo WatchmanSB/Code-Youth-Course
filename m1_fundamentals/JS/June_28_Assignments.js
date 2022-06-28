f3f173ea663e7eb3108fe8eeda20f11abea64c6c
WatchmanSB <69556611+WatchmanSB@users.noreply.github.com>
2022-06-28 12:42:14 -0400

    June 28th work for the day

---
+++ m1_fundamentals/JS/June_28_Assignments.js
@@ -0,0 +1,61 @@
+let nam = (first, last) => {
+    console.log("Hello, my name is", first, last)
+}
+
+nam ("Steven", "Belenchia")
+
+
+
+console.log (" ")
+
+
+let loc= (province, city, name) => {
+    console.log ("My name is", name, "and I live in", province, city)
+}
+
+loc("Ontario", "Ottawa", "Steven")
+
+
+console.log(" ")
+
+const number= (a,b) => { 
+    return (a*b)-2
+}
+
+console.log("This is the results of the multiplication", number(3,8))
+
+
+
+console.log(" ")
+
+const number2= (a,b) => { 
+    return (a/b)+2
+}
+
+console.log("This is the results of the multiplication", number2(10,22))
+
+
+
+
+
+console.log(" ")
+
+
+
+
+const colors= ["red", "green", "yellow", 2, 4, 6, 8, 10, true, false]
+console.log("The 7th element of the array is:", colors[6])
+
+
+
+
+
+const mylength= ["deng", "steven", "carter"];
+console.log("The length of the array is:", mylength.length);
+console.log("The first name of the array is:", mylength[0])
+
+
+
+
+
+
